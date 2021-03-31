ad0fd88fc42616591c1466bb0e1598f4ff0f890c
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-03-31 23:16:30 +0900

    show_action_added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    render json: @user, adapter: :json
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
