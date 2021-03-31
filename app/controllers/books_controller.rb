aa7a581e6cc5712911355f79e28b4d5434b12154
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-03-31 23:26:43 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
+  def show
+  render json: @book, adapter: :json
+  end
+
+  private
+  def set_book
+  @book = Book.find(params[:id])
+  end
 end
