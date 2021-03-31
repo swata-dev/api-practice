7917c735f6fca671214cc2fe0da5b2480f86ef9c
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-03-31 23:17:39 +0900

    seeds_put_in_db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+ User.create(
+  name: Faker::Name.unique.name,
+  email: Faker::Internet.unique.email,
+  token: SecureRandom.base64(30)
+ )
+end
